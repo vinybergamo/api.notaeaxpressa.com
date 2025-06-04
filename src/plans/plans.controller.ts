a068d7aa9fd3068b76891af8cf37b6ba5f95befd
vinybergamo <vinybergamo@gmail.com>
2025-06-04 06:52:03 -0300

    Adiciona endpoint de listagem paginada de planos com suporte a relações

--- src/plans/plans.controller.ts
+++ src/plans/plans.controller.ts
@@ -1,12 +1,24 @@
-import { Body, Controller } from '@nestjs/common';
+import { Body, Controller, Query } from '@nestjs/common';
 import { PlansService } from './plans.service';
 import { Endpoint } from '@/helpers/decorators/endpoint.decorator';
 import { Me } from '@/helpers/decorators/me.decorator';
 import { CreatePlanDto } from './dto/create-plan.dto';
+import { Paginate, PaginateQuery } from 'nestjs-paginate';
 
 @Controller('plans')
 export class PlansController {
   constructor(private readonly plansService: PlansService) {}
+
+  @Endpoint({
+    method: 'GET',
+  })
+  list(
+    @Me() user: UserRequest,
+    @Paginate() paginateQuery: PaginateQuery,
+    @Query('relations') relations: string,
+  ) {
+    return this.plansService.list(user, paginateQuery, relations || '');
+  }
 
   @Endpoint({
     method: 'POST',
--- src/plans/plans.service.ts
+++ src/plans/plans.service.ts
@@ -2,10 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { PlansRepository } from './plans.repository';
 import { Plan } from './entities/plan.entity';
 import { CreatePlanDto } from './dto/create-plan.dto';
+import { PaginateQuery } from 'nestjs-paginate';
 
 @Injectable()
 export class PlansService {
   constructor(private readonly plansRepository: PlansRepository) {}
+
+  async list(
+    user: UserRequest,
+    paginateQuery: PaginateQuery,
+    relations?: string,
+  ) {
+    return this.plansRepository.list(
+      user.id,
+      paginateQuery,
+      relations.split(/[;,\s]+/).filter(Boolean) || [],
+    );
+  }
 
   async create(user: UserRequest, createPlanDto: CreatePlanDto): Promise<Plan> {
     const plansCount = await this.plansRepository.count({
